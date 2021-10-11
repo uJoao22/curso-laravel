f7ddc2cf3be7636ce653566f31d216920bcc54a5
uJoao22 <88169092+uJoao22@users.noreply.github.com>
2021-10-11 16:15:56 -0300

    aula_06

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,21 @@
-<h1>Rota de produtos</h1>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="/css/style.css">
+        <script src="/js/script.js"></script>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+<body>
+    <h1>Tela de Produtos</h1>
+</body>
+</html>
